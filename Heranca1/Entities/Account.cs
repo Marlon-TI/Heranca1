fe5f0ed54c4f064c3db719cfcf73e3ae94819e34
Marlon <marlonsmoreira@gmail.com>
2020-05-09 17:38:22 -0300

    first commit

---
+++ Heranca1/Entities/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heranca1.Entities
+{
+    class Acount
+    {
+        public int Number { get; set; }
+        public String Holder { get; set; }
+        public double Balance { get; set; }
+
+        public Acount()
+        {
+        }
+
+        public Acount(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+    }
+}
